f8a11c3fe10b041613cc6266b382ac65bcac0460
jphilli3@gmail.com <tripphillips5@gmail.com>
2019-11-19 15:40:40 -0600

    Front End Stuff

--- db-front/src/app/app.component.ts
+++ db-front/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Guitar Hero';
+}
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './app.component.html',
+  template: `
+    <input (keyup)="input($event)">
+    <p>
+    <button (click)="search()">Search Chord</button>
+    </p>
+    <p>{{value}}</p>`
+})
+export class SearchButtonComponent {
+  value = '';
+  b = true;
+  search() {
+    
+  }
+  input(event: any) {
+    this.value = event.target.value;
+  }
 }
--- db-front/src/app/app.module.ts
+++ db-front/src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, SearchButtonComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SearchButtonComponent]
 })
 export class AppModule { }
